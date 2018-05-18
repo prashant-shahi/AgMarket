5acbf687ce263c13c6cd39200a637f07dc6313af
Prashant Shahi <CoolBoi567@gmail.com>
2018-05-18 15:44:40 +0530

    weather bug

--- weather.php
+++ weather.php
@@ -198,9 +198,6 @@ else {
 </div>
 <script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-<script type="text/javascript" src="vendor/bootstrap/js/popper.min.js"></script>
-<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script> 
-<!--===============================================================================================-->
 <script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
 <script type="text/javascript" src="js/main.min.js"></script>
