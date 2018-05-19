9d4ae5a3ef511efabfdc0186a566aa6222d93aa3
Prashant Shahi <CoolBoi567@gmail.com>
2018-05-20 00:16:57 +0530

    weather bug

--- weather.php
+++ weather.php
@@ -3,6 +3,7 @@ require_once('database.php');
 require_once('server.php');
 $json  =  file_get_contents("./weatherIcons.json");
 $weatherIcons  =  json_decode($json);
+
 function geticonprefix($code) {
 	global $weatherIcons;
 	$prefix = 'wi wi-';
@@ -15,6 +16,7 @@ function geticonprefix($code) {
 	$icon = $prefix . $icon;
 	return $icon;
 }
+
 function k_to_c($temp) {
 	if ( !is_numeric($temp) ) {
 		return false;
@@ -51,7 +53,7 @@ include('errors.php');
 
 if($counterrors!=0) {
 	?>
-	<section class="bg9 p-b-30">
+	<section class="bg9 p-t-30 p-b-50">
 		<div class="container">
 			<h3 class="bg6 text-center p-t-15 p-b-15">Error occured while processing your request.</h3>
 			<p class="text-center"><a href="index.php">Click here</a>, to go to Home Page</p>
@@ -68,82 +70,66 @@ else {
 					<h2 class = "text-center">Current Weather</h2><br/>
 				</div>
 				<?php
+	// $url = "https://api.openweathermap.org/data/2.5/weather?q=yelahanka&appid=3b3f916823675274f2fb80b7f4dd3d59";	
 				$url = "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=3b3f916823675274f2fb80b7f4dd3d59";
-				// $url = "https://api.openweathermap.org/data/2.5/weather?q=yelahanka&appid=3b3f916823675274f2fb80b7f4dd3d59";
 				$json  =  file_get_contents($url);
 				$currentdata  =  json_decode($json);
 
-		/*
-			{"coord":{"lon":77.6,"lat":13.1},
-			"weather":[
-				{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}
-			],
-			"base":"stations",
-			"main":{"temp":295.15,"pressure":1011,"humidity":100,"temp_min":295.15,"temp_max":295.15},
-			"visibility":6000, "wind":{"speed":1},
-			"clouds":{"all":40}, "dt":1526509800,
-			"sys":{
-				"type":1,"id":7823,"message":0.0039,"country":"IN",
-				"sunrise":1526430221,"sunset":1526476105
-				},
-			"id":1252758,"name":"Yelahanka","cod":200}
-		*/
-
 //			$today  =  date("F j, Y, g:i a");
-			$today = date("g:i:sa jS M, Y l");
-			$cityname  =  $currentdata->name; 
-			$weatherid = $currentdata->weather[0]->id;
-			$weathermain = $currentdata->weather[0]->main;
-			$weatherdescription = $currentdata->weather[0]->description;
-			$pressure = $currentdata->main->pressure;
-			$humidity = $currentdata->main->humidity;
-			$temp_current = $currentdata->main->temp;
-			$sunrise = new DateTime(date('r',$currentdata->sys->sunrise));
-			$sunrise = $sunrise->format("g:i a");
-			$sunset = new DateTime(date('r',$currentdata->sys->sunset));
-			$sunset = $sunset->format("g:i a")
-			?>
-			<div class="p-t-10 p-b-10 col-xs-12 col-md-12 col-lg-12">
-				<p><?php echo $today; ?></p>
-				<h3>Location: <?php echo $place." - ".$cityname; ?></h3>
-			</div>
-			<div class="col-xs-12 col-md-4 col-lg-4">
-				<?php
-				echo '<span class="center"><i class="' . geticonprefix($currentdata->weather[0]->id) . '" style="font-size:36px;"></i></span><br/>';
-				echo "Weather Main: <span class='weathermain'>" . ucwords($weathermain). "</span><br/>";
-				echo "Weather Description: <span class='weatherdesc'>" . ucwords($weatherdescription)."</span><br/>";
+				$today = date("g:i:sa jS M, Y l");
+				$cityname  =  $currentdata->name; 
+				$weatherid = $currentdata->weather[0]->id;
+				$weathermain = $currentdata->weather[0]->main;
+				$weatherdescription = $currentdata->weather[0]->description;
+				$pressure = $currentdata->main->pressure;
+				$humidity = $currentdata->main->humidity;
+				$temp_current = $currentdata->main->temp;
+				$sunrise = new DateTime(date('r',$currentdata->sys->sunrise));
+				$sunrise = $sunrise->format("g:i a");
+				$sunset = new DateTime(date('r',$currentdata->sys->sunset));
+				$sunset = $sunset->format("g:i a")
 				?>
-			</div>
-			<div class="col-xs-12 col-md-4 col-lg-4">
-				<?php
-				echo "Current Temp: " . k_to_c($temp_current) ." <span class='notranslate'>&deg;C</span><br>";
-				echo "Pressure: " . $pressure ." <span class='notranslate'>Pa</span><br>";
-				echo "Humidity: " . $humidity ." %<br>";
-				?>
-			</div>
-			<div class="col-xs-12 col-md-4 col-lg-4">
-				<?php
-				echo "Sunrise: <span class='notranslate'>" . $sunrise ."</span><br>";
-				echo "Sunset: <span class='notranslate'>" . $sunset ."</span><br>";
-				?>
+				<div class="p-t-10 p-b-10 col-xs-12 col-md-12 col-lg-12">
+					<p><?php echo $today; ?></p>
+					<h3>Location: <?php echo $place." - ".$cityname; ?></h3>
+				</div>
+				<div class="col-xs-12 col-md-4 col-lg-4">
+					<?php
+					echo '<span class="center"><i class="' . geticonprefix($currentdata->weather[0]->id) . '" style="font-size:36px;"></i></span><br/>';
+					echo "Weather Main: <span class='weathermain'>" . ucwords($weathermain). "</span><br/>";
+					echo "Weather Description: <span class='weatherdesc'>" . ucwords($weatherdescription)."</span><br/>";
+					?>
+				</div>
+				<div class="col-xs-12 col-md-4 col-lg-4">
+					<?php
+					echo "Current Temp: " . k_to_c($temp_current) ." <span class='notranslate'>&deg;C</span><br>";
+					echo "Pressure: " . $pressure ." <span class='notranslate'>Pa</span><br>";
+					echo "Humidity: " . $humidity ." %<br>";
+					?>
+				</div>
+				<div class="col-xs-12 col-md-4 col-lg-4">
+					<?php
+					echo "Sunrise: <span class='notranslate'>" . $sunrise ."</span><br>";
+					echo "Sunset: <span class='notranslate'>" . $sunset ."</span><br>";
+					?>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<hr/>
-<div class = 'weatherforecast p-b-30 p-t-20'>
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-xl-12">
-				<h2 class = "text-center">Weather Forecast</h2><br/>
+	<hr/>
+	<div class = 'weatherforecast p-b-30 p-t-20'>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-xl-12">
+					<h2 class = "text-center">Weather Forecast</h2><br/>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-2 col-xl-2"></div>
+				<div class="col-xs-12 col-sm-12 col-md-8 col-xl-8" id="openweathermap-widget-21" style="overflow: scroll;"></div>
+				<div class="col-xs-12 col-sm-12 col-md-2 col-xl-2"></div>
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-2 col-xl-2"></div>
-			<div class="col-xs-12 col-sm-12 col-md-8 col-xl-8" id="openweathermap-widget-21" style="overflow: scroll;"></div>
-			<div class="col-xs-12 col-sm-12 col-md-2 col-xl-2"></div>
 		</div>
 	</div>
-</div>
-<?php 
+	<?php 
 }
 ?>
 
@@ -205,8 +191,10 @@ else {
 		url: "https://api.openweathermap.org/data/2.5/weather?lat=<?php echo $lat; ?>&lon=<?php echo $lon; ?>&appid=3b3f916823675274f2fb80b7f4dd3d59",
 		dataType: 'json',
 		success: function(result){
-			var cityid = result["id"];
-			window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 21,cityid: cityid,appid: '3b3f916823675274f2fb80b7f4dd3d59',units: 'metric',containerid: 'openweathermap-widget-21',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
+			if(result['cod']=='200') {
+				var cityid = result["id"];
+				window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 21,cityid: cityid,appid: '3b3f916823675274f2fb80b7f4dd3d59',units: 'metric',containerid: 'openweathermap-widget-21',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
+			}
 		}
 	});
 </script>
